5da9bef2bae95d3ec7e5b86a0850639682b66cc7
jascinto leite aquino rego <165964313+gamertech811@users.noreply.github.com>
2025-11-24 13:59:08 -0300

    feat: concluirHabito

--- src/main/java/com/tresedemais/habitosDiarios/controllers/HabitoController.java
+++ src/main/java/com/tresedemais/habitosDiarios/controllers/HabitoController.java
@@ -41,6 +41,30 @@ public class HabitoController {
         return ResponseEntity.status(HttpStatus.OK).body(new MessageResponse("Hábito salvo com sucesso!"));
     }
 
+     @PostMapping("/concluir")
+    public ResponseEntity<MessageResponse> concluirHabitoDiario(@RequestBody IdRequest idRequest){
+//        IdRequest idRequest = new IdRequest(id);
+        Integer resposta = habServ.concluirHabitoDiario(idRequest);
+        switch (resposta){
+            case 1:
+                return ResponseEntity.status(HttpStatus.OK).body(new MessageResponse("Hábito concluído com sucesso!"));
+//                break;
+            case 2:
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageResponse("Hábito não encontrado!"));
+//                break;
+            case 3:
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResponse("Não é possível concluir este hábito depois do dia agendado"));
+//                break;
+            case 4:
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResponse("Não é possível concluir este hábito antes do dia agendado"));
+//                break;
+            default:
+                return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(new MessageResponse("Requisição com erro"));
+//                break; // ja tem return
+        }
+//        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(new MessageResponse("Requisição com erro"));
+    }
+
     @GetMapping("/data/{data}")
     public ResponseEntity<List<HabitoDiarioResponse>> findAllByDate(@PathVariable LocalDate data) {
         return ResponseEntity.ok(habServ.findAllByData(data));
